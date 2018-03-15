bc79fd0ccd202408b4ab6a7a1d8dd6bd6b7e4b5d
Abhinav Medikonda <v-abmedi@microsoft.com>
2018-03-15 16:50:28 -0700

    Add project files.

---
+++ ScanStackOverflow/Controllers/ScanController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScanStackOverflow.Controllers
+{
+    public class ScanController : Controller
+    {
+        // GET: Scan
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+    }
+}
---
+++ ScanStackOverflow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScanStackOverflow.MvcApplication" Language="C#" %>
